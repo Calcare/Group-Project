b876c3f393067ecdab1d6c21b93d35aafcdc2ae7
radentetuko <90023021+radentetuko@users.noreply.github.com>
2022-11-18 07:49:33 +0700

    Add files via upload
    tambah route halaman pilih makanan

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import Index from './pages/Index';
 import LogIn from './pages/LogIn';
-import Makanan from './pages/Makanan';
+import PilihMakanan from './pages/PilihMakanan';
 import SignUp from './pages/SingUp';
-import TrackCal from './pages/TrackCal';
 
 function App() {
   return (
@@ -14,8 +13,7 @@ function App() {
         <Route path='/SignUp'element={<SignUp/>} />
         <Route path='/LogIn'element={<LogIn/>} />
         <Route path='/Home'element={<HomePage/>} />
-        <Route path='/TrackCal' element={<TrackCal/>}/>
-        <Route path='/Makanan' element={<Makanan/>}/>
+        <Route path='/pilihMakanan'element={<PilihMakanan/>} />
       </Routes>
     </>
   );
